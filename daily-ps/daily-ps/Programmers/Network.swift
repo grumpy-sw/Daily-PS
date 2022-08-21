161250c9563e0f25b45abdc789f0d6c215a8e61e
Grumpy <sewoong2890@gmail.com>
2022-08-22 02:12:05 +0900

    22.08.15 / Practice Kit: 네트워크

--- daily-ps/daily-ps/Programmers/Network.swift
+++ daily-ps/daily-ps/Programmers/Network.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+final class Network {
+    func run() {
+        let testCase = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+        print(solution(4, [[1, 1, 0, 1], [1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 1]]))
+    }
+
+    
+    var visited: [Bool] = []
+    
+    func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+        var count: Int = 0
+        visited = Array(repeating: false, count: n)
+        
+        for index in 0..<n {
+            if !visited[index] {
+                count += 1
+                connectNetwork(n, computers, index)
+            }
+        }
+        return count
+    }
+    
+    func connectNetwork(_ n: Int, _ computers: [[Int]], _ start: Int) {
+        visited[start] = true
+        for index in 0..<n {
+            if !visited[index], computers[start][index] == 1 {
+                connectNetwork(n, computers, index)
+            }
+        }
+    }
+}
