4e18ff2b6ae288c6e69d26c97840deffbce0ab36
Grumpy <sewoong2890@gmail.com>
2022-09-28 12:43:04 +0900

    22.09.28 / 3053번: 택시 기하학

---
+++ daily-ps/daily-ps/Baekjoon/StepByStep-Geometry1/Baekjoon3053.swift
@@ -0,0 +1,16 @@
+//
+//  Baekjoon3053.swift
+//  daily-ps
+//
+//  Created by 박세웅 on 2022/09/28.
+//
+
+import Foundation
+
+final class Baekjoon3053 {
+    func run() {
+        let R = Double(readLine()!)!
+        print(R*R*Double.pi)
+        print(R*R*2)
+    }
+}
